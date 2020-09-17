5dd5d2014ce771b2b48a0559a0e8393f5fbe3594
Nikal Morgan <44351216+SlikNik@users.noreply.github.com>
2020-09-16 19:14:12 -0500

    Updated class views by adding .as_view()

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -22,14 +22,14 @@ from notification import views as notificationviews
 
 
 urlpatterns = [
-    path('', twitteruserviews.Index, name='homepage'),
+    path('', twitteruserviews.Index.as_view(), name='homepage'),
     path('login/', authenticateviews.login_view, name="loginview"),
     path('logout/', authenticateviews.logout_view, name="logoutview"),
-    path('signup/', twitteruserviews.SignUpView, name="signupview"),
+    path('signup/', twitteruserviews.SignUpView.as_view(), name="signupview"),
     path('profile/<str:username>/', twitteruserviews.user_detail, name='profile'),
-    path('tweet/<int:id>/', tweetviews.TweetDetail, name='tweetdetail'),
+    path('tweet/<int:id>/', tweetviews.TweetDetail.as_view(), name='tweetdetail'),
     path('home/', twitteruserviews.user_home_view, name='userhomepage'),
-    path('tweet/', tweetviews.TweetSubmit, name='tweetsubmit'),
+    path('tweet/', tweetviews.TweetSubmit.as_view(), name='tweetsubmit'),
     path('notification/', notificationviews.notified, name='notification'),
     path('follow/<str:username>/', twitteruserviews.follow_view, name='follow'),
     path('unfollow/<str:username>/', twitteruserviews.unfollow_view, name='unfollow'),
